55dab9c481810c5ce74d7904fa91a74c3fb18cc7
Giancarlo Scolaro <gian.scolaro@gmail.com>
2020-06-07 18:55:15 -0300

    fix get /points filtered

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -11,7 +11,7 @@ class PointsController {
             .map(item=>Number(item.trim()));
 
         const points = await knex('points')
-            .join('point_items','point_id', '=', 'point_items.point_id')
+            .join('point_items','points.id', '=', 'point_items.point_id')
             .whereIn('point_items.item_id', parsedItems)
             .where('city', String(city))
             .where('uf', String(uf))
